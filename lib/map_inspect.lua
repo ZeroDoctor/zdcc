3bc2de797f99d715e944e8dfea85921c8c53085f
zerodoctor <zerulus@gmail.com>
2023-12-22 00:04:45 -0600

    add initial web structure

---
+++ lib/map_inspect.lua
@@ -0,0 +1,30 @@
+
+local track = require("lib.track_move")
+
+local script = {}
+
+function script:init(move)
+	self.track = move or track
+end
+
+function script:inspect()
+
+end
+
+function script:inspectUp()
+
+end
+
+function script:inspectDown()
+
+end
+
+local function add_block(self, data)
+	local block = {
+		location = self.track.location,
+		data = data
+	}
+end
+
+return script
+
